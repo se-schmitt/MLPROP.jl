b61fd4b51ff61450ffc1640797d9701f031694bd
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-01-13 12:58:32 +0100

    [clean] first code stripping

--- lib/ChemBERTa/src/tokenizer/textencoder.jl
+++ lib/ChemBERTa/src/tokenizer/textencoder.jl
@@ -1,63 +1,9 @@
 abstract type AbstractTransformerTextEncoder <: AbstractTextEncoder end
 
-function Base.show(io::IO, e::AbstractTransformerTextEncoder)
-    print(io, "$(nameof(typeof(e)))(\n├─ ")
-    print(io, e.tokenizer)
-    _io = IOContext(io, :compact => true)
-    for name in fieldnames(typeof(e))
-        (name == :tokenizer || name == :process) && continue
-        val = getfield(e, name)
-        if !isnothing(val)
-            if val isa Base.Fix1{typeof(nestedcall)}
-                print(_io, ",\n├─ ", name, " = nestedcall")
-                val.x isa ComposedFunction || print(_io, '(')
-                FuncPipelines.show_pipeline_function(_io, val.x)
-                val.x isa ComposedFunction || print(_io, ')')
-            elseif !(val isa Pipelines) && val isa Function
-                print(_io, ",\n├─ ", name, " = ")
-                FuncPipelines.show_pipeline_function(_io, val)
-            else
-                print(_io, ",\n├─ ", name, " = ", val)
-            end
-        end
-    end
-    print(IOContext(io, :pipeline_display_prefix => "  ╰─ "), ",\n└─ process = ", e.process, "\n)")
-end
-
-"""
-    struct TrfTextEncoder{
-        T <: AbstractTokenizer,
-        V <: AbstractVocabulary{String},
-        C, A, EP, OP, DP, TP
-    } <: AbstractTransformerTextEncoder
-        tokenizer::T
-        vocab::V
-        config::C
-        annotate::A
-        process::EP
-        onehot::OP
-        decode::DP
-        textprocess::TP
-    end
-
-The general text encoder. `TrfTextEncoder` has multiple fields that can modify the encode/decode process:
-
-1. `.annotate` (default to `TextEncoders.annotate_strings`): Annotate the input string for the tokenizer,
- e.g. `String` would be treated as a single sentence, not a single word.
-2. `.process` (default to `TextEncodeBase.nestedcall(TextEncoders.string_getvalue)`): The pre-process
- function applied to the tokenization results, e.g. adding special `end-of-sentence` token, computing attention mask...
-3. `.onehot` (default to `TextEncoders.lookup_fist`): Apply onehot encoding on the preprocess result,
- the default behavior takes the first element from the proprocess result and applies onehot encoding.
-4. `.decode` (default to `identity`): The function that converts each token id back to string. This can
- be used to handle some tokenizers that use a different set of vocabulary such as gpt2's byte-level vocabulary.
-5. `.textprocess` (default to `TextEncodeBase.join_text`): the function that joins the `decode`-d result
- in complete sentence(s).
-
-"""
 struct TrfTextEncoder{
     T <: AbstractTokenizer,
     V <: AbstractVocabulary{String},
-    C, A, EP, OP, DP, TP
+    C, A, EP, OP
 } <: AbstractTransformerTextEncoder
     tokenizer::T
     vocab::V
@@ -65,37 +11,10 @@ struct TrfTextEncoder{
     annotate::A
     process::EP
     onehot::OP
-    decode::DP
-    textprocess::TP
 end
 
-"""
-    TrfTextEncoder(
-        tokenizer     :: AbstractTokenizer ,
-        vocab         :: AbstractVocabulary{String} ,
-        [ annotate    =  TextEncoders.annotate_string ,
-        [ process     =  TextEncodeBase.nestedcall(TextEncoders.string_getvalue) ,
-        [ onehot      =  TextEncoders.lookup_first ,
-        [ decode      =  identity ,
-        [ textprocess =  TextEncodeBase.join_text, ]]]]]
-        ; config...)
-
-Constructor of `TrfTextEncoder`. All keyword arguments are store in the `.config` field.
-"""
-TrfTextEncoder(tokenizer::AbstractTokenizer, vocab::AbstractVocabulary{String}; kws...) =
-    TrfTextEncoder(tokenizer, vocab, annotate_strings; kws...)
-TrfTextEncoder(tokenizer::AbstractTokenizer, vocab::AbstractVocabulary{String}, annotate; kws...) =
-    TrfTextEncoder(tokenizer, vocab, annotate, nestedcall(string_getvalue); kws...)
-TrfTextEncoder(tokenizer::AbstractTokenizer, vocab::AbstractVocabulary{String}, annotate, process; kws...) =
-    TrfTextEncoder(tokenizer, vocab, annotate, process, lookup_first; kws...)
 TrfTextEncoder(tokenizer::AbstractTokenizer, vocab::AbstractVocabulary{String}, annotate, process, onehot; kws...) =
-    TrfTextEncoder(tokenizer, vocab, annotate, process, onehot, identity; kws...)
-TrfTextEncoder(tokenizer::AbstractTokenizer, vocab::AbstractVocabulary{String}, annotate, process, onehot, decode;
-               kws...) = TrfTextEncoder(tokenizer, vocab, annotate, process, onehot, decode, join_text; kws...)
-function TrfTextEncoder(tokenizer::AbstractTokenizer, vocab::AbstractVocabulary{String},
-                        annotate, process, onehot, decode, textprocess; kws...)
-    return TrfTextEncoder(tokenizer, vocab, values(kws), annotate, process, onehot, decode, textprocess)
-end
+    TrfTextEncoder(tokenizer, vocab, values(kws), annotate, process, onehot)
 
 for name in fieldnames(TrfTextEncoder)
     (name == :tokenizer || name == :vocab) && continue
@@ -124,35 +43,13 @@ end
 
 @inline _membercall(f, e, x) = !(f isa Pipelines) && static_hasmethod(f, Tuple{typeof(e), typeof(x)}) ? f(e, x) : f(x)
 
-TextEncodeBase.process(::Type{<:TrfTextEncoder}) = nestedcall(string_getvalue)
 TextEncodeBase.tokenize(e::TrfTextEncoder, x) = getfield(e, :tokenizer)(_membercall(getfield(e, :annotate), e, x))
 TextEncodeBase.process(e::TrfTextEncoder, x) = _membercall(getfield(e, :process), e, x)
 TextEncodeBase.lookup(e::TrfTextEncoder, x) = _membercall(getfield(e, :onehot), e, x)
-TextEncodeBase.decode(e::TrfTextEncoder, x) = _membercall(getfield(e, :decode), e, TextEncodeBase.decode_indices(e, x))
-TextEncodeBase.decode_text(e::TrfTextEncoder, x) = _membercall(getfield(e, :textprocess), e, TextEncodeBase.decode(e, x))
-
-TextEncodeBase.decode_indices(e::TrfTextEncoder, x) = decode_indices(e, x)
-decode_indices(e::TrfTextEncoder, i::Union{Integer, OneHotArray, AbstractArray{<:Integer}}) =
-    lookup(String, getfield(e, :vocab), i)
-function decode_indices(e::TrfTextEncoder, x::AbstractArray)
-    if ndims(x) < 2
-        i = argmax(x)
-    else
-        amax = reshape(argmax(x; dims=1), Base.tail(size(x)))
-        i = selectdim(reinterpret(reshape, Int, amax), 1, 1)
-    end
-    return decode_indices(e, i)
-end
-
-annotate_strings(x::AbstractString) = Sentence(x)
-annotate_strings(x::Vector{<:AbstractString}) = Batch{Sentence}(x)
-annotate_strings(x::Vector{<:Vector{<:AbstractString}}) = Batch{Batch{Sentence}}(x)
-annotate_strings(x::Vector{<:Vector{<:Vector{<:AbstractString}}}) = Batch{Batch{Batch{Sentence}}}(x)
-
 TextEncodeBase.onehot_encode(e::TrfTextEncoder, x) = lookup(OneHot, getfield(e, :vocab), x)
 
-lookup_first(e::TrfTextEncoder, x) = TextEncodeBase.onehot_encode(e, x)
-lookup_first(e::TrfTextEncoder, x::Tuple) = (TextEncodeBase.onehot_encode(e, x[1]), Base.tail(x)...)
+annotate_strings(x::AbstractString) = Sentence(x)
+
 function lookup_first(e::TrfTextEncoder, x::NamedTuple{name}) where name
     xt = Tuple(x)
     return NamedTuple{name}((TextEncodeBase.onehot_encode(e, xt[1]), Base.tail(xt)...))
@@ -177,8 +74,6 @@ function TransformerTextEncoder(tokenizef, words; trunc = nothing, startsym = "<
         annotate_strings,
         identity,
         lookup_first,
-        identity,
-        join_text,
     )
 
     builder(e) = begin
--- lib/ChemBERTa/src/tokenizer/tokenizer.jl
+++ lib/ChemBERTa/src/tokenizer/tokenizer.jl
@@ -32,9 +32,3 @@ TextTokenizer() = TextTokenizer(TextEncodeBase.DefaultTokenization())
 
 TextEncodeBase.tokenization(tkr::TextTokenizer) = tkr.tokenization
 
-container_eltype(::Type{<:Batch{T}}) where T<:Union{SentenceStage, DocumentStage} = Vector{TokenStage}
-container_eltype(::Type{<:Batch{Batch{T}}}) where T<:SentenceStage = Vector{Vector{TokenStage}}
-container_eltype(::Type{<:Batch{Batch{Batch{T}}}}) where T<:SentenceStage = Vector{Vector{Vector{TokenStage}}}
-container_reducef(::Type{<:Batch}) = push!
-container_f(::T) where T = (container_reducef(T), MutableLinkedList{container_eltype(T)}())
-
