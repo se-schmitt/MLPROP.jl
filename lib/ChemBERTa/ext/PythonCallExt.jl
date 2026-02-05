197f6ccdf451dd8b57718a523bb640a4d448ce52
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-02-05 22:35:04 +0100

    [feat] finalize extensions

--- lib/ChemBERTa/ext/PythonCallExt.jl
+++ lib/ChemBERTa/ext/PythonCallExt.jl
@@ -6,6 +6,13 @@ using PythonCall
 const chem = Ref{Py}()
   
 function __init__()
+    if Base.get_extension(Main.ChemBERTa, :RDKitMinimalLibExt) isa Module
+        @warn """
+        Function `ChemBERTa.canonicalize` defined by `PythonCallExt` and `RDKitMinimalLibExt`!
+        `PythonCallExt` is used.
+        """
+    end
+
     chem[] = pyimport("rdkit.Chem")
 end
 
@@ -19,10 +26,14 @@ function _get_smiles(mol)
 end
 
 # Canonization
-function ChemBERTa.canonicalize(smiles::AbstractString; is_canonical=false)
+function _canonicalize(smiles)
     mol = _get_mol(smiles)
     string(mol) == "None" && error("Invalid SMILES: '$(smiles)'!")
     return string(_get_smiles(mol))
+end
+
+function ChemBERTa.canonicalize(smiles::AbstractString; kwargs...)
+    return _canonicalize(smiles)
 end
 
 end
