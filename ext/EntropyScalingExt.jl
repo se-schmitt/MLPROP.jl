696319fc923204d592dc5ff2883ad03a0fff957b
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-02-09 16:16:11 +0100

    [fix] error in EntropyScalingExt

--- ext/EntropyScalingExt.jl
+++ ext/EntropyScalingExt.jl
@@ -3,7 +3,9 @@ module EntropyScalingExt
 using MLPROP
 using EntropyScaling
 
-function MLPROP.SEB(components, es_model::AbstractESModel; kwargs..., p=1e5)
+const ES = EntropyScaling
+
+function MLPROP.SEB(components, es_model::ES.AbstractEntropyScalingModel; p=1e5, kwargs...)
     wrapper = ESModelWrapper(es_model, p)
     return SEB(components, wrapper; kwargs...)
 end
