75e170272cfe5d860f29e4df1058fd2a943388e4
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-02-10 14:44:40 +0100

    [revise] first revision of SEB code

--- src/models/SEB.jl
+++ src/models/SEB.jl
@@ -1,117 +1,87 @@
 
-function M(descs::Dict)
-    #Calculating molecular weight of given molecul
-    return get(descs,"exactmw",0)*(10^(-3))
+struct SEBParam{T}
+    M::T
+    b_ij::T
 end
 
-function R(descs::Dict)
-    #Calculating boolean parameter determing the presence of rings of given molecul
-    return !(get(descs,"NumRings",0) == 0)
-end
-
-function r_het(descs::Dict)
-    #Calculating the ratio of Number of Heteroatoms to number of heavy atoms of given molecul
-    return (get(descs,"NumHeteroatoms",0)/get(descs,"NumHeavyAtoms",0))
-end
-
-function r_acc(descs::Dict)
-    #Calculating ratio of hydrogenbondacceptors to number of heavy atoms of given molecul
-    return (get(descs,"NumHBA",0)/get(descs,"NumHeavyAtoms",0))
-end
-
-function r_don(descs::Dict)
-    ##Calculating ratio of hydrogenbonddonors to number of heavy atoms of given molecul
-    return (get(descs,"NumHBD",0)/get(descs,"NumHeavyAtoms",0))
-end
-
-function r_hal(SMILES::String,descs::Dict)
-    #Calculating ratio of halogenatoms to heavy atoms of given molecule
-    return descs["NumHalogens"]/get(descs,"NumHeavyAtoms",0)
-end
-
-struct SEBParam
-    "Molar mass in `kg/mol`"
-    M
-    "Correction factor for the Stokes-Einstein equation"
-    b_ij
+struct SEB{M,T}
+    components::Vector{AbstractString}
+    param::SEBParam{T}
+    vis_model::M
 end
 
 """
-SEB()
+    SEB(SMILE_i, SMILE_j, vis_model)
 
-The SEB-modell provides a way to calculate the diffusioncoefficents at infinite dilution in a binary
-mixture utilizing a neural network to boost the results of the Stokes-Einstein-equation.
+## Description
 
-# Parameters
-
-'components'::vector[2x1] Vector containing SMILES of solute and solution
-
-'param': M::float64 Molar mass of solute in kg/mol
-         b_ij::float64 Boostingfactor for SE-Equation
-
-'vis_model'::func(T) Function for modelling dynamic Viscosity of solution
-
-# Constructor SEB(SMILE_i::String,SMILE_j::String,vis_model)
-
+SEB model for calculating diffusion coefficients at infinite dilution in a binary mixture.
+The model predicts the boosting factor for the Stokes-Einstein equation using a neural network
+trained on molecular descriptors of the solute and solvent.
 """
-struct SEB{M}
-  components::Vector#{<AbstractString}
-  param::SEBParam
-  vis_model::M
-end
-
-function SEB(SMILE_i::String,SMILE_j::String,eta_fun)
-    #Loading weights and bias from nn_parameters.jld2
+function SEB(SMILE_i::AbstractString, SMILE_j::AbstractString, eta_fun)
+    #TODO use Clapeyron style
+    # Loading weights and bias from nn_parameters.jld2
     path_nn_parameters = joinpath(DB_PATH, "SEB", "weights_bias_true.jld2")
     nn_parameters = load(path_nn_parameters)["Weights_Bias_SEB"]
     
-    #Processing SMILES to get molecular descriptors used in neural net
-    desc_i,desc_j = get_descriptors(SMILE_i), get_descriptors(SMILE_j)
-  
-    X_i_ini=[M(desc_i);r_acc(desc_i);r_don(desc_i);r_het(desc_i); r_hal(SMILE_i,desc_i);R(desc_i)]
-    X_j_ini=[M(desc_j);r_acc(desc_j);r_don(desc_j);r_het(desc_j); r_hal(SMILE_j,desc_j);R(desc_j)]
-    if SMILE_i == "O" || SMILE_i == "[2H]O[2H]"
-       X_i_ini=[M(desc_i);0.5;0.5;r_het(desc_i); r_hal(SMILE_i,desc_i);R(desc_i)]
-    end
-    if SMILE_j == "O" || SMILE_j == "[2H]O[2H]"
-        X_j_ini=[M(desc_j);0.5;0.5;r_het(desc_j); r_hal(SMILE_j,desc_j);R(desc_j)]
-    end
-    
-    input=vcat(X_i_ini,X_j_ini)
-    
-    #input=[M(desc_i);M(desc_j);R(desc_i);R(desc_j);r_het(desc_i);r_het(desc_j);r_hal(SMILE_i,desc_i);r_hal(SMILE_j,desc_j)
-    #;r_acc(desc_i);r_acc(desc_j);r_don(desc_i);r_don(desc_j)]
-    MW=M(desc_i)
-    #b_ij Berechnung => Modell
-    b_ij=0
-    b_ij_mean=0
+    # Processing SMILES to get molecular descriptors used in neural net
+    desc_i, desc_j = get_descriptors(SMILE_i), get_descriptors(SMILE_j)
+
+    mw_i = desc_i["exactmw"] * 1e-3
+    mw_j = desc_j["exactmw"] * 1e-3
+
+    is_water_i = SMILE_i == "O" || SMILE_i == "[2H]O[2H]"
+    is_water_j = SMILE_j == "O" || SMILE_j == "[2H]O[2H]"
+
+    X_i_ini = [
+        mw_i;
+        is_water_i ? 0.5 : desc_i["NumHBA"] / desc_i["NumHeavyAtoms"];
+        is_water_i ? 0.5 : desc_i["NumHBD"] / desc_i["NumHeavyAtoms"];
+        desc_i["NumHeteroatoms"] / desc_i["NumHeavyAtoms"];
+        desc_i["NumHalogens"] / desc_i["NumHeavyAtoms"];
+        desc_i["NumRings"] != 0;
+    ]
+    X_j_ini = [
+        mw_j;
+        is_water_j ? 0.5 : desc_j["NumHBA"] / desc_j["NumHeavyAtoms"];
+        is_water_j ? 0.5 : desc_j["NumHBD"] / desc_j["NumHeavyAtoms"];
+        desc_j["NumHeteroatoms"] / desc_j["NumHeavyAtoms"];
+        desc_j["NumHalogens"] / desc_j["NumHeavyAtoms"];
+        desc_j["NumRings"] != 0;
+    ]
+
+    input = vcat(X_i_ini, X_j_ini)
+    MW = mw_i
+    b_ij_mean = 0.0
 
     #Initializing Neural Net using Lux
-    NN = Chain(Dense(12 => 32,relu),Dense(32 => 16,relu),Dense( 16 => 1,softplus))
-    #keys=["SEB_3";"SEB_7";"SEB_9";"SEB_12";"SEB_17";"SEB_19";"SEB_33";"SEB_42";"SEB_49";"SEB_55"]
+    NN = Chain(Dense(12 => 32, relu), Dense(32 => 16, relu), Dense(16 => 1, softplus))
 
     # Looping over parameter-sets to determine mean b_ij
-    for (key,wb) in nn_parameters
+    for (_, wb) in nn_parameters
 
         #Setting up the weights and bias of the neural net using Lux-Synatx
-        st=(layer_1=NamedTuple(),layer_2=NamedTuple(),layer_3=NamedTuple())
-        ps=((layer_1=(weight=wb[2],bias=vec(wb[1]))),
-        (layer_2=(weight=wb[4],bias=vec(wb[3]))),
-        (layer_3=(weight=wb[6],bias=vec(wb[5]))))
+        st = (layer_1 = NamedTuple(), layer_2 = NamedTuple(), layer_3 = NamedTuple())
+        ps = (
+            (layer_1 = (weight = wb[2], bias = vec(wb[1]))),
+            (layer_2 = (weight = wb[4], bias = vec(wb[3]))),
+            (layer_3 = (weight = wb[6], bias = vec(wb[5]))),
+        )
         #applying neural net with given weights and bias to calculate b_ij
-        b_ij, st = NN(input,ps,st)
-        b_ij_mean += first(b_ij)  
+        b_ij, st = NN(input, ps, st)
+        b_ij_mean += only(b_ij)
     end
-    b_ij_mean=b_ij_mean/length(nn_parameters)
+    b_ij_mean /= length(nn_parameters)
 
     # Constructing SEBParam-datastructure
-    paramSEB=SEBParam(MW,b_ij_mean)
+    paramSEB = SEBParam(MW, b_ij_mean)
 
-
-    return SEB([SMILE_i;SMILE_j],paramSEB,eta_fun)
+    return SEB([String(SMILE_i), String(SMILE_j)], paramSEB, eta_fun)
 end
 
 Base.broadcastable(x::SEB) = Ref(x)
+
 """
 Diffusion
 Diffusion calculates the diffusioncoefficent, using the Stokes-Einstein-equation and multiplying 
@@ -121,7 +91,7 @@ the result with the Boostingfactor
 'p': Pressure in Pa
 'T': Temperature in Kelvin
 """
-function Diffusion(model::SEB,p,T)
+function Diffusion(model::SEB, p, T)
     # Initialitzing constants required for Stokes-Einstein-equation
     k_b = 1.380649e-23
     roh_i= 1050
@@ -131,7 +101,7 @@ function Diffusion(model::SEB,p,T)
     N_A=6.02214076e23
     r_i=((3*f*M_i)/(4*pi*roh_i*N_A))^(1/3)
     D_SEE_ij_infdil = (k_b*T)/(6*pi*visc_j*r_i)
-    return D_SEE_ij_infdil*model.param.b_ij[1]
+    return D_SEE_ij_infdil * model.param.b_ij
 
 end
 
