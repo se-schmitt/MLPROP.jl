fc2172f1c0b1e92d17b0fa57e23f1825ae1a8095
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-02-12 15:48:33 +0100

    [fix] did not precompile

--- src/models/models.jl
+++ src/models/models.jl
@@ -3,4 +3,16 @@ include("utils.jl")
 include("grappa.jl")
 include("HANNA/hanna.jl")
 
-MLPROP_MODELS = Union{GRAPPA, ogHANNA}
+# Show method
+const MLPROP_MODELS = Union{GRAPPA, ogHANNA}
+function Base.show(io::IO, mime::MIME"text/plain", model::MLPROP_MODELS)
+    print(io, nameof(typeof(model)))
+    length(model) == 1 && println(io, " with 1 component:")
+    length(model) > 1 && println(io, " with ", length(model), " components:")
+    CL.show_pairs(io,CL.component_list(model))
+    
+    CL.show_info(io,model)
+    CL.show_params(io,model)
+    CL.show_reference_state(io,model)
+    CL.may_show_references(io,model)
+end
